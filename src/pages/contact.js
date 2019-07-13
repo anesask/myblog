2d403a86b548048b53e3ccb44befc6ef6d9028f8
anesask <anes.mulalic@hotmail.com>
2019-07-13 23:17:54 +0200

    Banally ReCaptcha Fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -26,7 +26,7 @@ const Contact = () => (
         <input type="text" name="email" id="email" placeholder="Email" required/>
         <input type="text" name="subject" id="subject" placeholder="Subject"  required/>
         <textarea name="message" id="message" placeholder="Write your message..." required/>
-        <div data-netlify-recaptcha></div>
+        <div data-netlify-recaptcha="true"></div>
         <input type="submit" value="Send Message" className="send-button"/>            
       </form>
 
